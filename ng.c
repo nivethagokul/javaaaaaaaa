9159db665e696a99375a2a0683406033c777e56f
nivethagokul <umagovindan22@gmail.com>
2016-08-08 14:47:38 +0530

    Add files via upload

---
+++ ng.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#define MAXLEN 100
+ 
+void reverseString(char*, int, int);
+void reverseWords(char*, int);
+int main()
+{
+  char string[MAXLEN] = "Nivetha Gokul";
+  printf("Input String: %s\n", string);
+  reverseString(string, 0, strlen(string) - 1);
+  reverseWords(string, strlen(string));
+  printf("Output String: %s\n", string);
+}
+ 
+void reverseString(char* buffer, int startIndex, int endIndex)
+{
+ 
+  int ch, i, j;
+  for (i = startIndex, j = endIndex; i < j; i++, j--)
+  {
+ch = buffer[i];
+buffer[i] = buffer[j];
+  buffer[j] = ch;
+  }
+}
+ 
+void reverseWords(char* buffer, int strLength)
+{
+  if (*buffer == '\0') // check for null
+return;
+ 
+
+  
+  
